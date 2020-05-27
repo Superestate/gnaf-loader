2d831a53f35d5e0ec0d5f0319971e4babc09aadb
Philip Evans <philip.fevans@gmail.com>
2020-05-27 16:12:01 +1000

    Modified restore script

--- supporting-files/restore-gnaf-admin-bdys.sh
+++ supporting-files/restore-gnaf-admin-bdys.sh
@@ -1,6 +1,14 @@
+
 #!/usr/bin/env bash
+# requires that psql client is installed and PGPASSWORD var exists in ~/.pqpass
 
-psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+echo "Installing postgis extension"
+psql -h $DBHOST -d $DBNAME -U $DBUSER -w -p 5432 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-/Applications/Postgres.app/Contents/Versions/11/bin/pg_restore -Fc -d geo -p 5432 -U postgres /Users/hugh.saalmans/git/minus34/gnaf-202005.dmp
-/Applications/Postgres.app/Contents/Versions/11/bin/pg_restore -Fc -d geo -p 5432 -U postgres /Users/hugh.saalmans/git/minus34/admin-bdys-202005.dmp
+echo "Running gnaf db restore from pg_dump file"
+/usr/bin/pg_restore -Fc -h $DBHOST -d $DBNAME -w -p 5432 -U $DBUSER ../gnaf-dump-files/gnaf-202005.dmp
+
+echo "Running gnaf admin boundary db restore from pg_dump file"
+/usr/bin/pg_restore -Fc -h $DBHOST -d $DBNAME -w -p 5432 -U $DBUSER ../gnaf-dump-files/admin-bdys-202005.dmp
+
+
